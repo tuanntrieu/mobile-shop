e9f26b1532aa3160bbf5ac5240025f1ec092a003
tuanntrieu <trieudangtuan18032003@Gmail.com>
2024-09-01 22:08:32 +0700

    :art: login,register ui

--- src/main/resources/static/client/js/main.js
+++ src/main/resources/static/client/js/main.js
@@ -11,3 +11,40 @@ var swiper = new Swiper(".mySwiper", {
         clickable: true,
     }
 });
+
+//auth-service
+$(document).ready(function() {
+    $('#togglePassword').click(function() {
+        const passwordField = $('#password');
+        const passwordFieldType = passwordField.attr('type');
+        const toggleIcon = $('#togglePassword');
+
+        if (passwordFieldType === 'password') {
+            passwordField.attr('type', 'text');
+            toggleIcon.removeClass('fa fa-eye-slash').addClass('fa fa-eye');
+        } else {
+            passwordField.attr('type', 'password');
+            toggleIcon.removeClass('fa fa-eye').addClass('fa fa-eye-slash');
+
+        }
+    });
+
+    $('#togglePasswordRegister').click(function() {
+        const passwordRegister = $('#registerPassword');
+        const passwordRepeatRegister = $('#registerRepeatPassword');
+        const passwordFieldType = passwordRegister.attr('type');
+        const toggleRegisterIcon = $('#togglePasswordRegister');
+
+        if (passwordFieldType === 'password') {
+            passwordRegister.attr('type', 'text');
+            passwordRepeatRegister.attr('type', 'text');
+            toggleRegisterIcon.removeClass('fa fa-eye-slash').addClass('fa fa-eye');
+        } else {
+            passwordRegister.attr('type', 'password');
+            passwordRepeatRegister.attr('type', 'password');
+            toggleRegisterIcon.removeClass('fa fa-eye').addClass('fa fa-eye-slash');
+
+        }
+    });
+});
+//auth-service
